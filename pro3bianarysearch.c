02000253bf9e793d257f91e8e8a74acac696e738
RoronoaZoro-dev <132890094+RoronoaZoro-dev@users.noreply.github.com>
2025-04-23 21:24:04 +0530

    Update pro3bianarysearch.c

--- pro3bianarysearch.c
+++ pro3bianarysearch.c
@@ -17,7 +17,7 @@ int main() {
         printf("%d\t", a[i]);
     }
 
-    // Bubble sort
+    
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
             if (a[j] > a[j + 1]) {
